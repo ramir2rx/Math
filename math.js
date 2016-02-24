c7e299e1747f8e4a2bb08cecc2935a9fee2ddeba
ramir2rx <ramir2rx@dukes.jmu.edu>
2016-02-24 09:54:41 -0500

    tested all operators

--- math.js
+++ math.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
-var args = process.argv.slice(2);
-var operator = args[0].toLowerCase();
-args = args.slice(1);
+// var args = process.argv.slice(2);
+// var operator = args[0].toLowerCase();
+// args = args.slice(1);
 
 
-if (args.length < 2) {
-	console.log("missing args");
-}
-else {
-	console.log(args.reduce(calculate));
-}
-
-function calculate(a, b) {
+// if (args.length < 2) {
+// 	console.log("missing args");
+// }
+// else {
+// 	console.log(args.reduce(calculate));
+// }
+exports.calculate = function (a, b) {
 	if(operator == 'add') {
 		return +a + +b;
 	}
@@ -28,3 +27,4 @@ function calculate(a, b) {
 		return "invalid operator: " + operator;
 	}
 }
+
--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,7 +1,23 @@
-describe("A simple program that does aritmetic", function() {
-  it("can add two numbers", function() {
-  	operator = "add";
-    expect(calculate(3,5)).toBe(8);
-  });
-});
+  var math = require('../math');
 
+  describe("A simple program that does aritmetic", function() {
+  	it("can add two numbers", function() {
+  		operator = "add";
+  		expect(math.calculate(3,5)).toBe(8);
+  	});
+
+  	it("can subtract two numbers", function() {
+  		operator = "subtract";
+  		expect(math.calculate(3,5)).toBe(-2);
+  	});
+
+  	it("can multilply two numbers", function() {
+  		operator = "multiply";
+  		expect(math.calculate(3,5)).toBe(15);
+  	});
+
+  	it("can divide numbers", function() {
+  		operator = "divide";
+  		expect(math.calculate(6,2)).toBe(3);
+  	});
+  });
